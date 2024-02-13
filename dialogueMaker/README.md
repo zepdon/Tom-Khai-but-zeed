6b3764bec397e5a4b7a756f94549076e333dabac
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2024-02-13 13:11:43 +0000

    dialogueMaker

---
+++ dialogueMaker/README.md
@@ -0,0 +1,18 @@
+# DialogueMaker
+
+## Documentation
+Coming Soon
+
+## How to use
+1. Download `src` folder
+2. Move into `src` folder
+3. Add scenes and choices in `main.cpp`
+4. Compile with this command
+```
+g++ -Wall main.cpp dialogueMaker.cpp -o main -std=c++11
+./main
+```
+## Example
+![cover](cover.png)
+![result](result.png)
+
---
+++ dialogueMaker/src/dialogueMaker.h
@@ -0,0 +1,50 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Scene;
+
+struct Option {
+  string text;
+  int sceneId;
+};
+
+class Scene {
+  int id;
+  string dialogue;
+  vector<Option> options;
+
+  public:
+    Scene(int id, string dialogue);
+
+    void printScene(); 
+
+    void addOption(int choiceId, string text, int nextSceneId);
+
+    int chooseOption(int choice);
+
+    int getId();
+};
+
+class Game {
+  vector<Scene*> scenes;
+  Scene* currentScene;
+
+  void printEndGame();
+
+  public:
+    void addScene(int id, string dialogue);
+
+    void addOption(int sceneId, vector<Option> options);
+
+    void start();
+  
+    void setCurrentScene(int id);
+
+    void printCurrentScene();
+
+    void askForChoice();
+
+    bool gameEnded();
+};
+
