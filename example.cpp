1f0daf3235c0c81d9f256896d02a452971b3901d
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2024-02-20 23:22:41 +0700

    Keep example dialogue

---
+++ example.cpp
@@ -0,0 +1,32 @@
+#include "dialogueMaker/dialogueMaker.h"
+
+int main() {
+  // Create scenes
+  Game::addScene("begin", "You are in [r]a forest[/]. [ul]What do you do?[/]");
+  Game::addScene("2", "You see a bear. What do you do?");
+  Game::addScene("3", "You see a river. What do you do?");
+  Game::addScene("4", "You see a bridge. What do you do?");
+  Game::addScene("bad_ending", "You died.", true);
+  Game::addScene("nice_ending", "You survived.", true);
+
+  // Add options to scenes
+  Game::addOption("begin", {{"Go left", "2"}, {"Go right", "3"}, {"Go straight", "4"}});
+
+  Game::addOption("2", {
+    {"Run", "begin"},
+    {"Fight", "bad_ending"}
+  });
+
+  Game::addOption("3", {
+    {"Swim", "bad_ending"},
+    {"Walk around", "begin"}
+  });
+
+  Game::addOption("4", {
+    {"Cross it", "nice_ending"},
+    {"Jump off", "bad_ending"}
+  });
+
+  // Game loop
+  Game::runGame("begin");
+}
